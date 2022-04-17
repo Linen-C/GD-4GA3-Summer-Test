67482a48f8e18d98c56cd1c5946dbace89d0bb62
Linen-C <2016002@s.asojuku.ac.jp>
2022-04-17 21:50:47 +0900

    2022-04-17：実装いろいろ変えたほうがいい気がするゥ～

--- GD_Sum_testproject/Assets/Script/PlayerCTRL.cs
+++ GD_Sum_testproject/Assets/Script/PlayerCTRL.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerCTRL : MonoBehaviour
 {
-    public float speed;
+    public float moveSpeed;
     public float timeLimit;
+    public int defoAttackTiming;
+    public GameObject attackObject;
 
     private float counter = 0;
+    private int attackTiming;
 
     Rigidbody2D body;
 
@@ -16,24 +19,32 @@ public class PlayerCTRL : MonoBehaviour
     {
         body = GetComponent<Rigidbody2D>();
         counter = timeLimit;
+        attackTiming = defoAttackTiming;
     }
 
     // 60/1秒単位で呼び出されるトコ
     void Update()
     {
         body.velocity = new Vector2(
-            Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+            Input.GetAxis("Horizontal") * moveSpeed, Input.GetAxis("Vertical") * moveSpeed);
 
         TestShot();
 
         if (counter < 0)
         {
-            Debug.Log("カウント");
+            attackTiming--;
             counter = timeLimit;
+            Debug.Log("カウント");
         }
         else
         {
             counter = counter - Time.deltaTime;
+        }
+
+        if (attackTiming == 0)
+        {
+            Attack();
+            attackTiming = defoAttackTiming;
         }
     }
 
@@ -41,6 +52,18 @@ public class PlayerCTRL : MonoBehaviour
     void TestShot()
     {
         if (Input.GetMouseButtonDown(0)) { Debug.Log("左クリック");}
+    }
+
+    void Attack()
+    {
+        Debug.Log("攻撃");
+        
+        //attackObject
+        GameObject attack = 
+        (GameObject)Instantiate(
+            attackObject,
+            new Vector3(transform.position.x,transform.position.y,0),
+            Quaternion.identity);
     }
 
 }
